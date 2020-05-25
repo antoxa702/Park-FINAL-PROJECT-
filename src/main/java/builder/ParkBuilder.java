444d17d9bba09fa4f2683d9bd4284dfb762c4617
Anton Tolstoy <antoxa_702@mail.ru>
2020-05-25 15:12:36 +0300

    Add ParkBuilder

---
+++ src/main/java/builder/ParkBuilder.java
@@ -0,0 +1,33 @@
+package builder;
+
+import entity.Park;
+
+public class ParkBuilder {
+	
+	Park park;
+	
+	public ParkBuilder() {
+		park = new Park();
+	}
+	
+	public void createNewPark() {
+		park = new Park();
+	}
+	
+	public void withId(int id) {
+		park.setId(id);
+	}
+	
+	public void withName(String parkName) {
+		park.setName(parkName);
+	}
+	
+	public void withArea (double parkArea) {
+		park.setArea(parkArea);
+	}
+	
+	public Park build() {
+		return park;
+	}
+
+}
