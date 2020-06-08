099a74bff23be1aea251abc83093e2087afc71e0
Anton Tolstoy <antoxa_702@mail.ru>
2020-06-08 14:08:43 +0300

    Add methods to UserDAO

--- src/main/java/builder/UserBuilder.java
+++ src/main/java/builder/UserBuilder.java
@@ -94,5 +94,4 @@ public class UserBuilder {
 	public User build(){
 		return this.user;
 	}
-
 }
--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -7,4 +7,7 @@ import java.util.List;
 
 public interface UserDao extends AbstractDao<User>{
 	List<User> getAllUsers() throws DAOException;
+	User getByLogin(String login) throws DAOException;
+	List<User> getUsersByParkId(int parkId) throws DAOException;
+	List<User> getUsersByUserTypeId(int userTypeId) throws DAOException;
 }
